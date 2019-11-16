e59165c8532f3e21345c8c5ee2407155cfc6d67a
Arthit Suriyawongkul <arthit@gmail.com>
2019-11-16 20:12:08 +0000

    - Replace comparisons with None from "==" to "is" and from "!=" to "is not" - Drop unnecessary comparison, follows Python convention of default True value if a value is not None - Formatted with black

--- Utility/Utils.py
+++ Utility/Utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 def getWordTag(wordTag):
     if wordTag == "///":
         return "/", "/"
@@ -7,25 +8,28 @@ def getWordTag(wordTag):
     if index == -1:
         return wordTag, None
     word = wordTag[:index].strip()
-    tag = wordTag[index + 1:].strip()
+    tag = wordTag[index + 1 :].strip()
     return word, tag
 
+
 def getRawText(inputFile, outFile):
-    out = open(outFile, "w")
-    sents = open(inputFile, "r").readlines()
-    for sent in sents:
-        wordTags = sent.strip().split()
-        for wordTag in wordTags:
-            word, tag = getWordTag(wordTag)
-            out.write(word + " ")
-        out.write("\n")
-    out.close()
-    
+    with open(inputFile, "r") as f:
+        sents = f.readlines()
+
+    with open(outFile, "w") as out:
+        for sent in sents:
+            wordTags = sent.strip().split()
+            for wordTag in wordTags:
+                word, _ = getWordTag(wordTag)
+                out.write(word + " ")
+            out.write("\n")
+
+
 def readDictionary(inputFile):
     dictionary = {}
-    lines = open(inputFile, "r").readlines()
+    with open(inputFile, "r") as f:
+        lines = f.readlines()
     for line in lines:
         wordtag = line.strip().split()
         dictionary[wordtag[0]] = wordtag[1]
     return dictionary
-
