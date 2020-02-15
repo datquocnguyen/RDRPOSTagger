895fd3d3ef32fb8fe8af867ee897874767620934
Zhaoyang Lyu <43465228+ZhaoyangLyu@users.noreply.github.com>
2020-02-15 16:40:59 +0800

    change directory structure, add ignore

--- Utility/LexiconCreator.py
+++ Utility/LexiconCreator.py
@@ -2,10 +2,10 @@
 
 import os
 import sys
-os.chdir("../")
-sys.setrecursionlimit(100000)
-sys.path.append(os.path.abspath(""))
-os.chdir("./Utility")
+# os.chdir("../")
+# sys.setrecursionlimit(100000)
+# sys.path.append(os.path.abspath(""))
+# os.chdir("./Utility")
 
 import re
 from Utility.Utils import getWordTag
